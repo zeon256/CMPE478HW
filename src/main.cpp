4c29a23accf9a2fe0576b73d55acc44bf2a5c124
Budi Syahiddin <me@inve.rs>
2023-11-13 22:21:36 +0300

    feat: CsvRow

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,56 @@
 #include <cstdio>
+#include <sstream>
 #include <vector>
 
+class Benchmark {};
+
 class CsvRow {
-private:
-    int M;
-    std::string openmp_scheduling_method;
-    int chunk_sz;
-    double t1;
-    double t2;
-    double t4;
-    double t8;
-    double s2;
-    double s4;
-    double s8;
+   private:
+    const int M;
+    const std::string openmp_scheduling_method;
+    const int chunk_sz;
+    const double t1;
+    const double t2;
+    const double t4;
+    const double t8;
+    const double s2;
+    const double s4;
+    const double s8;
+
+   public:
+    CsvRow(int M, std::string openmp_scheduling_method, int chunk_sz, double t1,
+           double t2, double t3, double t4, double t8)
+        : M(M),
+          openmp_scheduling_method(openmp_scheduling_method),
+          chunk_sz(chunk_sz),
+          t1(t1),
+          t2(t2),
+          t4(t4),
+          t8(t8),
+          s2(t2 / t1),
+          s4(t4 / t1),
+          s8(t8 / t1) {}
+
+    std::string to_string() const {
+        std::stringstream ss;
+        ss << M << ", " << openmp_scheduling_method << ", " << chunk_sz << ", "
+           << t1 << ", " << t2 << ", " << t4 << ", " << t8 << ", " << s2 << ", "
+           << s4 << ", " << s8;
+        return ss.str();
+    }
 };
 
-auto generate_csv_row() -> void {}
+void generate_csv_row() {}
 
-auto generate_primes(const int N) -> std::vector<int> {
-    std::vector<int> prime(N, 2);
-    
+void generate_primes(const int N) {
+    std::vector<int> prime = {2};
+
     // we add 2 because all priems are odd (except for 2)
     // so we can step += 2 instead of +=1
-    for (int n = 3; count < N; n += 2) {
+    for (int n = 3; n < N; n += 2) {
         bool isPrime = true;
 
-        for (int k = 0; k < count && prime[k] * prime[k] <= n; ++k) {
+        for (int k = 0; prime[k] * prime[k] <= n; ++k) {
             // check if number n can be broken down using another prime
             // ie can be prime factorized
             if (n % prime[k] == 0) {
@@ -34,16 +59,13 @@ auto generate_primes(const int N) -> std::vector<int> {
             }
         }
 
-        if (isPrime) prime[count++] = n;
+        if (isPrime) prime.push_back(n);
     }
-    
-    return prime;
+
+    for (int c : prime) printf("%d\n", c);
 }
 
-
-auto main() -> int {
-
-    for(int c : generate_primes(50)) printf("%d\n", c);
-
+int main() {
+    generate_primes(50);
     return 0;
 }
