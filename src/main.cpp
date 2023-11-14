2f86fc917f832df3112641754d327fd0a908df44
Budi Syahiddin <me@inve.rs>
2023-11-15 00:00:16 +0300

    Co-authored-by: ahmadazfar <ahmadazfar@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <utility>
+
+#include "omp.h"
 
 class Benchmark {
    private:
@@ -11,9 +12,57 @@ class Benchmark {
     const int step;
 
     static void find_primes_static(const int N, const int T, const int C) {
+        omp_set_num_threads(T);
         std::vector<int> primes{2};
 
-        for (int n = 3; primes.size() < N; n += 2) {
+#pragma omp parallel for schedule(static, C) private(primes)
+        for (int n = 3; n < N; n += 2) {
+            bool isPrime = true;
+
+            for (int i = 0; i < primes.size(); ++i) {
+                int prime = primes[i];
+                if (prime * prime > n) break;
+
+                // if number can be prime factorized => not prime
+                if (n % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) primes.push_back(n);
+        }
+    }
+
+    static void find_primes_dynamic(const int N, const int T, const int C) {
+        omp_set_num_threads(T);
+        std::vector<int> primes{2};
+
+#pragma omp parallel for schedule(dynamic, C) private(primes)
+        for (int n = 3; n < N; n += 2) {
+            bool isPrime = true;
+
+            for (int i = 0; i < primes.size(); ++i) {
+                int prime = primes[i];
+                if (prime * prime > n) break;
+
+                // if number can be prime factorized => not prime
+                if (n % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) primes.push_back(n);
+        }
+    }
+
+    static void find_primes_guided(const int N, const int T, const int C) {
+        omp_set_num_threads(T);
+        std::vector<int> primes{2};
+
+#pragma omp parallel for schedule(guided, C) private(primes)
+        for (int n = 3; n < N; n += 2) {
             bool isPrime = true;
 
             for (int i = 0; i < primes.size(); ++i) {
@@ -36,11 +85,13 @@ class Benchmark {
      *
      * @tparam F Function type
      * @param m Number of primes to find
+     * @param T Number of threads
+     * @param C Chunk size
      * @param f Function to benchmark
      */
     template <typename F>
-    inline long long time_benchmark(const int m, const int T, const int C,
-                                    F f) {
+    inline double time_benchmark(const int m, const int T, const int C,
+                                 const F f) {
         std::chrono::steady_clock::time_point begin =
             std::chrono::steady_clock::now();
 
@@ -52,10 +103,10 @@ class Benchmark {
         auto time_taken =
             std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
-        return time_taken.count();
+        return (double)time_taken.count();
     }
 
-    void matrix_to_csv(std::vector<std::array<std::string, 10>> matrix) {
+    void matrix_to_csv(std::vector<std::array<std::string, 10>> const& matrix) {
         std::stringstream ss;
 
         for (auto row : matrix) {
@@ -70,6 +121,34 @@ class Benchmark {
         outfile.close();
     }
 
+    template <typename F>
+    void run_and_insert_to_csv_row(
+        std::string scheduler_name, const int chunk, const int m,
+        std::vector<std::array<std::string, 10>>& csv, F f) {
+        std::vector<double> timings;
+
+        // num threads
+        for (int t = 1; t <= 8; t *= 2) {
+            timings.push_back(time_benchmark(m, t, chunk, f));
+        }
+
+        // append speed up to timings
+        const int T_SZ = timings.size();
+        for (int i = 1; i < T_SZ; ++i) {
+            timings.push_back((double)timings[0] / timings[i]);
+        }
+
+        std::array<std::string, 10> timings_str = {
+            std::to_string(m), scheduler_name, std::to_string(chunk)};
+
+        for (int i = 0; i < timings.size(); ++i) {
+            timings_str[i + 3] = std::to_string(timings[i]);
+        }
+
+        // insert to csv
+        csv.push_back(std::array<std::string, 10>(timings_str));
+    }
+
    public:
     Benchmark(int max_m, int step) : max_m(max_m), step(step) {}
 
@@ -80,37 +159,27 @@ class Benchmark {
         csv.push_back({"M", "OpenMP Loop Scheduling Method", "Chunk Size", "T1",
                        "T2", "T4", "T8", "S2", "S4", "S8"});
 
-        for (int m = step; m <= max_m; m += step) {
+        std::vector<int> chunk_size = {2, 16, 32, 64, 128, 256};
+
+        for (int m = 0; m <= max_m; m += step) {
             printf("[M = %d] Starting benchmark\n", m);
 
-            std::vector<long long> timings = {
-                time_benchmark(m, 1, 2, find_primes_static),
-                time_benchmark(m, 2, 2, find_primes_static),
-                time_benchmark(m, 4, 2, find_primes_static),
-                time_benchmark(m, 8, 2, find_primes_static),
-            };
-
-            // append speed up to timings
-            const int T_SZ = timings.size();
-            for (int i = 1; i < T_SZ; ++i) {
-                timings.push_back((double)timings[0] / timings[i]);
+            for (int chunk : chunk_size) {
+                run_and_insert_to_csv_row("static", chunk, m, csv,
+                                          find_primes_static);
+                run_and_insert_to_csv_row("dynamic", chunk, m, csv,
+                                          find_primes_dynamic);
+                run_and_insert_to_csv_row("guided", chunk, m, csv,
+                                          find_primes_guided);
             }
-
-            // insert to csv
-            csv.push_back(
-                {std::to_string(m), "static", "1", std::to_string(timings[0]),
-                 std::to_string(timings[1]), std::to_string(timings[2]),
-                 std::to_string(timings[3]), std::to_string(timings[4]),
-                 std::to_string(timings[5]), std::to_string(timings[6])});
         }
 
-        // dont make a copy, move it instead to save memory
-        matrix_to_csv(std::move(csv));
+        matrix_to_csv(csv);
     }
 };
 
 int main() {
-    auto bench = Benchmark(10000, 1000);
+    auto bench = Benchmark(100000, 1000);
     bench.run_benchmark();
 
     return 0;
